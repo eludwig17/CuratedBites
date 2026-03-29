f8773eae75202f119f1330aacc9a5ecc4d3ed619
eludwig17 <elijahludwigyt@gmail.com>
2026-03-29 16:57:53 -0700

    fix to run flask app via "flask run" command

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, jsonify
 from routes.restaurants import restaurantsBP
 from routes.reviews import reviewsBP
 from routes.users import usersBP
-
+import os
+os.environ.setdefault("FLASK_APP", "app.py")
+FLASK_APP="app.py"
+FLASK_DEBUG=1
 app = Flask(__name__)
 app.register_blueprint(restaurantsBP)
 app.register_blueprint(reviewsBP)
