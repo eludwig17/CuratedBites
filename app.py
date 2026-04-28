2ff65edc18b50998499349d8ed77a9dd27b49556
eludwig17 <elijahludwigyt@gmail.com>
2026-04-27 21:54:40 -0700

    added insights BP

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, jsonify
 from routes.restaurants import restaurantsBP
 from routes.reviews import reviewsBP
 from routes.users import usersBP
+from routes.insights import insightsBP
 FLASK_APP="app.py"
 FLASK_DEBUG=1
 app = Flask(__name__)
 app.register_blueprint(restaurantsBP)
 app.register_blueprint(reviewsBP)
 app.register_blueprint(usersBP)
+app.register_blueprint(insightsBP)
 
 @app.route('/')
 def index():
@@ -18,6 +20,7 @@ def index():
             "restaurants": "/api/restaurants",
             "reviews": "/api/reviews",
             "users": "/api/users",
+            "insights": "/api/insights",
         }
     }), 200
 
